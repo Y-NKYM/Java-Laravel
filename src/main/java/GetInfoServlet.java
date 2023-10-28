2087de6b8605f04ca48c759245fdff42dd6059df
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-10-28 13:02:32 +0900

    [Add] webservlet wild urlpattern

--- src/main/java/GetInfoServlet.java
+++ src/main/java/GetInfoServlet.java
@@ -6,12 +6,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 /**
  * WebServlet指定ルートを記述でサーブレットの読み込み。
  * JSPファイルの拡張子記述でサーブレット読み込むことなくJSPを表示。
  */
-@WebServlet("/getinfo")
+@WebServlet("/hello/*")
 public class GetInfoServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 	    throws IOException, ServletException {
@@ -22,7 +21,11 @@ public class GetInfoServlet extends HttpServlet {
 	    if(name != null) {
 	    	req.setAttribute("name", name);
 	    }
-	    RequestDispatcher rd = req.getRequestDispatcher("getInfo.jsp");
+	    String servInfo = req.getServletPath();
+	    String pathInfo = req.getPathInfo();
+	    System.out.println(servInfo.substring(1));
+	    System.out.println(pathInfo);
+	    RequestDispatcher rd = req.getRequestDispatcher("/getInfo.jsp");
 	    rd.forward(req, res);
 	}
 }
